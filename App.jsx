5130f33fbde4dfdcc916d52dacd9b14d9e88755e
Curanda <103290697+Curanda@users.noreply.github.com>
2023-09-01 16:44:25 +0000

    Create App.jsx

---
+++ App.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import "./App.css";
+import Accordion from "./assets/Accordion";
+export default function App() {
+    return (
+        <div className={'body'}>
+            <Accordion />
+        </div>
+    );
+}
